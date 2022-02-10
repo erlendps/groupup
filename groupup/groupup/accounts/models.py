aed5d1dfda56e78992873b61c56088b637ff9c67
Dorthea Munch-Ellingsen Selnes <dmselnes@stud.ntnu.no>
2022-02-10 13:28:29 +0100

    #15 Add GroupUpUser model
    Co-authored-by: Erlend Paulsen Skaaden <erlendps@stud.ntnu.no>

--- groupup/groupup/accounts/models.py
+++ groupup/groupup/accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+def user_image_path(instance, filename):
+    user_name = instance.user.username
+    extension = filename.rsplit('.', 1)[1]
+    return 'users/{0}.{1}'.format(user_name, extension)
+
+class Interest(models.Model):
+    name = models.CharField(max_length=30)
+
+class GroupUpUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to = user_image_path)
+    interests = models.ManyToManyField(Interest)
+
