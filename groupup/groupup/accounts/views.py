696da31ce41fb42df7268c1027b8deeed625921d
Nilashan Arul <71449959+hattrickjjmo@users.noreply.github.com>
2022-02-10 14:26:18 +0100

    #3 - deleted loginForm

--- groupup/groupup/accounts/views.py
+++ groupup/groupup/accounts/views.py
@@ -5,4 +5,4 @@ from django.http    import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, "accounts/loginForm.html")
+    return render(request, "accounts/index.html")
