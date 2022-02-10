30d0c2143a576ad97f84840e159dda83e84a3266
larsberg <lbe@stud.ntnu.no>
2022-02-10 14:51:32 +0100

    begin on register

--- groupup/groupup/accounts/views.py
+++ groupup/groupup/accounts/views.py
@@ -4,5 +4,9 @@ from django.http    import HttpResponse
 
 
 # Create your views here.
+def register(request):
+    return render(request, "accounts/registerForm.html")
+
+    # Create your views here.
 def index(request):
     return render(request, "accounts/loginForm.html")
--- groupup/groupup/urls.py
+++ groupup/groupup/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', home.hello),
-    path("login/", include("groupup.accounts.urls")),
+    path("", include("groupup.accounts.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
