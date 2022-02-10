e64fb717c8fb72429e1132503f7a6d6770bc0da4
Nilashan Arul <71449959+hattrickjjmo@users.noreply.github.com>
2022-02-10 14:48:25 +0100

    #3 - updated user login

--- groupup/groupup/settings.py
+++ groupup/groupup/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'groupup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
