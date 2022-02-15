3fd86e0470a294a216a8d3e233ee19309671624c
Nilashan Arul <71449959+hattrickjjmo@users.noreply.github.com>
2022-02-15 13:19:22 +0100

    #7 - created landing page template

--- groupup/groupup/accounts/views.py
+++ groupup/groupup/accounts/views.py
@@ -4,5 +4,5 @@ from django.http    import HttpResponse
 
 
 # Create your views here.
-def index(request):
-    return render(request, "accounts/index.html")
+def landing(request):
+    return render(request, "accounts/templates/landing.html")
