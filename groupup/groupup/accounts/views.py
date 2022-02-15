a290b97ffbc912757561402aba5b1c6a83343b3b
Dorthea Munch-Ellingsen Selnes <dmselnes@stud.ntnu.no>
2022-02-15 16:09:14 +0100

    #10 Create view and html for group site
    Co-authored-by: Erlend Paulsen Skaaden <erlendps@stud.ntnu.no>

--- groupup/groupup/accounts/views.py
+++ groupup/groupup/accounts/views.py
@@ -1,11 +1,16 @@
 from re import template
 from django.shortcuts import render
 from django.http    import HttpResponse
+from .models import UserGroup
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 def homepage(request):
     return render(request, "accounts/home.html")
 
+@login_required
 def group_site(request, pk):
-    return render(request, "accounts/group_site.html")
+    group = UserGroup.objects.get(pk=pk)
+    context = {"group": group}
+    return render(request, "accounts/group_site.html", context)
