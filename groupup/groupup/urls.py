55b6c15d5dbdffd6b1c39f1e56536ddb9de5ce94
Erlend Paulsen Skaaden <erlendps@stud.ntnu.no>
2022-02-17 14:35:27 +0100

    3 - finished login with auth

--- groupup/groupup/urls.py
+++ groupup/groupup/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 from views import home
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
