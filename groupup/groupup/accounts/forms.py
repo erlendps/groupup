26d181eec96451b06f948c20d48b5a2abefe379d
Matias Flatebø <matiasfl@stud.ntnu.no>
2022-02-17 14:25:10 +0100

    #4 lagd form og sender til db

--- groupup/groupup/accounts/forms.py
+++ groupup/groupup/accounts/forms.py
@@ -1,8 +1,9 @@
+from unittest.util import _MAX_LENGTH
 from django import forms
 
-class LoginForm(forms.form):
-    username = forms.CharField(label="Username", max_length=100)
-    password = forms.PasswordInput(label="Password")
-
-class RegisterForm(forms.form):
-    username = forms.CharField(label="Username", max_length=100)
+class RegisterForm(forms.Form):
+    username = forms.CharField(label="username", max_length=100)
+    password = forms.CharField(label="password", widget=forms.PasswordInput)
+    profile_pic = forms.ImageField(label="profile_pic")
+    interests = forms.BooleanField(label="interests")
+    dateofbirth = forms.DateField(label="dateofbirth")
--- groupup/groupup/accounts/views.py
+++ groupup/groupup/accounts/views.py
@@ -1,32 +1,27 @@
 from re import template
 from django.shortcuts import render
 from django.http    import HttpResponse, HttpResponseRedirect
+
+from .forms import RegisterForm
 from .models import GroupUpUser
 from django.contrib.auth.models import User
-
+from .forms import RegisterForm
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        profile_pic = request.POST.get('profile_pics')
-        interests = request.POST.get('interests')
-        password = request.POST.get('password')
-
-        user = User(username = username, password = password)
-        #Sjekk for unikt navn, hvis den ikke finnes lagre den
-        if len(list(User.objects.filter(username=username))) != 0:
-            #Feilmelding
-            return
-        groupupuser = GroupUpUser(user, profile_pic, interests)
-
-        #Kan lage en FOR-løkke for interest
-        groupupuser.save()
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = User(username = form.username, password = form.password)
+            groupupuser = GroupUpUser(user, form.profile_pic, form.interests)
+            groupupuser.save()
 
 
         return HttpResponseRedirect('/home')
-    print(request.POST.get)
-    return render(request, "registration/registerForm.html")
+
+    else:
+        form = RegisterForm()
+        return render(request, 'registration/registerForm.html', {'form': form})
 
     # Create your views here.
 def index(request):
