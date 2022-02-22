2c7acca363fcef8765160e7db5cb968184f8849a
Erlend Paulsen Skaaden <erlendps@stud.ntnu.no>
2022-02-22 16:32:42 +0100

    fix a bug where it linked to a view that didnt exist

--- groupup/groupup/accounts/urls.py
+++ groupup/groupup/accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.homepage, name="homepage"),
     path("register/", views.register, name = "register"),
-    path("login/", views.index, name = "index"),
+    path("login/", views.homepage, name = "index"),
     path("groups/<int:pk>/", views.group_site, name="group_site"),
     path("groups/<int:pk>/matches", views.group_matches, name="group_matches"),
 ]
