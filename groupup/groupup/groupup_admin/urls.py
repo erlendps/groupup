03871f391c2e431e8934ebc15d9ebfb11630e6e0
Erlend Paulsen Skaaden <erlendps@stud.ntnu.no>
2022-02-26 17:07:01 +0100

    #23 fix links for admin pages

--- groupup/groupup/groupup_admin/urls.py
+++ groupup/groupup/groupup_admin/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = "group_admin"
 
 urlpatterns = [
-    path('group/<int:pk>', views.group_browsing),
-    path('sendmatchrequest/<int:pk>', views.send_match_request),
-    path('viewmatchrequests/<int:pk>', views.view_match_requests),
-    path('handle_requests/<int:pk>', views.handle_match_request),
+    path('groups/', views.all_groups, name='groups'),
+    path('groups/<int:pk>', views.group_browsing, name='group_browsing'),
+    path('sendmatchrequest/<int:pk>', views.send_match_request, name='send_match'),
+    path('viewmatchrequests/<int:pk>', views.view_match_requests, name='view_match'),
+    path('handle_requests/<int:pk>', views.handle_match_request, name='handle_request'),
 ] 
