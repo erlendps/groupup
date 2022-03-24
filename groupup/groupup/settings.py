0bda198958b6b9cdddc31fbf085ca970dd653db9
Trym Lund Flogard <trym@flogard.no>
2022-03-24 14:26:30 +0100

    update settings

--- groupup/groupup/settings.py
+++ groupup/groupup/settings.py
@@ -29,6 +29,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["pu.mvmt.no"]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "https://pu.mvmt.no"
+]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "https://pu.mvmt.no"
+]
+
 
 # Application definition
 
